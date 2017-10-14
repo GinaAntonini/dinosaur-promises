ca597e57cfe836ac15318476a2379bf0e5f7c404
Gina Antonini <ginamantonini@gmail.com>
2017-10-14 11:09:37 -0500

    base card styling

--- javascripts/dom.js
+++ javascripts/dom.js
@@ -4,8 +4,25 @@ const outputDiv = $('#dinosaur');
 
 const domString = (dinosaur) => {
 	var dinoString = "";
-		dinoString += `<div>`;
+		dinoString += `<div class=${dinosaur.info === 'Carnivore' ? 'card-bad' : 'card-good'}>`;
 		dinoString += `<h1>${dinosaur.type}</h1>`;
+		dinoString += `<h4>${dinosaur.bio}</h4>`;
+		if (dinosaur.info === 'Carnivore') {
+			dinoString += `<h4>Has some tasty snacks.</h4>`;
+		} else {
+			dinoString += `<h4>Has some adorable(debatable) friends.</h4>`;
+		}
+		dinoString += `<div class='card-holder'>`;
+		dinosaur.snacks.forEach((cat) => {
+			dinoString += `<div class='card'>`;
+			dinoString += `<h5>${cat.name}</h5>`;
+			dinoString += `<div class='card-img'>`;
+			dinoString += `<img src=${cat.imageUrl}>`;
+			dinoString += `</div>`;
+			dinoString += `<p class='card-description'>${cat.specialSkill}</p>`;
+			dinoString += `</div>`;
+		});
+		dinoString += `</div>`;
 		dinoString += `</div>`;
 	writeToDom(dinoString);
 };
